51f7b034722aab514a2d4d55c9549c1c22ec1048
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2021-02-10 14:39:08 +0700

    Initialize app

--- fabeliotest/settings.py
+++ fabeliotest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zg=o@9ed%8-vj04uyo9+#&_zo*@jf=4$iv(u!alac8zh*0*8mf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
